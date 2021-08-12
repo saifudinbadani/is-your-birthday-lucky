86c4e24003e1474cb5fbc9411d0c0317ce6ada3f
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-12 17:32:44 +0800

    javascript done

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ var birthdayInput=document.querySelector('#birthdayInput');
 var luckyNumber=document.querySelector('#luckyNumber');
 var checkButton=document.querySelector('#checkButton');
 var output=document.querySelector('#result');
-var birthdayTotal = 0;
-function birthdayTotalCalculator(){
-    for(i=0; i<Number(birthdayInput.value.length); i++){
-        birthdayTotal = birthdayTotal + Number(birthdayInput.value[i]);
+var birthdayTotal = '';
+var sum = 0;
 
+
+
+function birthdaySumCalculator(){
+    var birthdayArray= birthdayInput.value.split('-');
+    
+
+    for(i=0; i<birthdayArray.length; i++){
+        birthdayTotal = birthdayTotal + birthdayArray[i];
+        
     }
-}
+
+
+    for (i=0; i<birthdayTotal.length; i++){
+            sum = sum + Number(birthdayTotal.charAt(i));
+            
+        } 
+        
+    }
+    
+    
+    
+    
+
 
 checkButton.addEventListener('click', function luckCalculator(){
-    birthdayTotalCalculator();
-    if (birthdayTotal % Number(luckyNumber.value) === 0){
+    birthdaySumCalculator();
+    if (sum % Number(luckyNumber.value) === 0){
         output.innerText = 'You are lucky!'
     } else
     {output.innerText = 'You are unlucky!'}
